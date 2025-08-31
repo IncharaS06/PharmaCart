8170e1f308da0637a0b478dc12df8afb33b29be4
Incharas06 <incharas1906@gmail.com>
2025-08-31 23:18:35 +0530

    v1

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          flexibleSpace: Center(
+            child: Text(
+              'PharmaCart',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
